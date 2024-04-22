e0d5ca1139d2b3d43526205b6ea4c6fbcbe1eb66
Akhil N <4khiln@gmail.com>
2024-04-22 19:03:22 +0530

    feat: admin dashbaord sales chart, top 5 products ,top 5 categorys

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,6 +1,8 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const Order = require('../models/orderModel');
+const Products = require('../models/productModel');
+const { isDefined } = require('razorpay/dist/utils/razorpay-utils');
 
 // LOGIN AND LOGOUT SECTON START //
 
@@ -44,24 +46,189 @@ const adminLogOut = async (req, res) => {
 
 // LOGIN AND LOGOUT SECTON END //
 
+
 // DASHBOADR SECTON START //
 
 
 // lodDashboard
 const loadDashboard = async (req, res) => {
     try {
-        const result = await Order.aggregate([
-            { $unwind: '$products' },
-            { $match: { 'products.status': 'delivered' } },
-            { $group: { _id: null, count: { $sum: 1 } } }
-        ]);
-        const orderscount = result.length > 0 ? result[0].count : 0;
-        res.render('dashboard', { orderscount });
-    } catch (error) {
+        // Orders count 
+        const orderscount = await Order.countDocuments({ status: "placed" });
 
+        // Product count 
+        const productscount = await Products.countDocuments({ isDeleted: false });
+
+        // Total revenue 
+        const revenueResult = await Order.aggregate([
+            { $match: { status: "placed" } },
+            { $unwind: "$products" },
+            { $match: { "products.status": "delivered" } },
+            {
+                $group: {
+                    _id: null,
+                    totalRevenue: { $sum: { $multiply: ["$products.price", "$products.quantity"] } }
+                }
+            }
+        ]);
+        const totalRevenue = revenueResult.length ? revenueResult[0].totalRevenue : 0;
+
+        // Monthly Revenue and Orders Count
+        const monthlyResult = await Order.aggregate([
+            { $match: { status: "placed" } },
+            { $unwind: "$products" },
+            { $match: { "products.status": "delivered" } },
+            {
+                $group: {
+                    _id: { month: { $month: "$date" } },
+                    totalRevenue: { $sum: { $multiply: ["$products.price", "$products.quantity"] } },
+                    count: { $sum: 1 }
+                }
+            }
+        ]);
+
+        // Create an array to hold monthly earnings
+        const monthlyEarnings = Array.from({ length: 12 }, (_, i) => {
+            const monthData = monthlyResult.find(month => month._id.month === i + 1);
+            return {
+                month: i + 1,
+                totalRevenue: monthData ? monthData.totalRevenue : 0,
+                count: monthData ? monthData.count : 0
+            };
+        });
+
+        const currentDate = new Date();
+        const currentMonthRevenue = monthlyEarnings.find(month => month.month === currentDate.getMonth() + 1)?.totalRevenue || 0;
+
+
+        // Monthly Users Count
+        const monthlyUser = await User.aggregate([
+            {
+                $match: {
+                    verified: { $exists: true }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        month: { $month: '$dateJoined' },
+
+
+                    },
+                    count: { $sum: 1 }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    month: '$_id.month',
+
+                    count: 1
+                }
+            },
+            {
+                $sort: {
+                    month: 1,
+
+                }
+            }
+        ]);
+
+      
+        const userMonthly = Array.from({ length: 12 }, (_, i) => ({
+            month: i + 1,
+            count: 0
+        }));
+        const monthlyUserCounts = userMonthly.map((defaultMonth) => {
+            const foundMonth = monthlyUser.find((monthlyData) => monthlyData.month === defaultMonth.month);
+            return {
+                month: defaultMonth.month,
+                count: foundMonth ? foundMonth.count : 0,
+            };
+        });
+
+                // Top 10 
+                const topProducts = await Order.aggregate([
+                    { $match: { status: "placed" } },
+                    { $unwind: "$products" },
+                    { $match: { "products.status": "delivered" } },
+                    { $group: { _id: "$products.productId", quantity: { $sum: "$products.quantity" } } },
+                    { $sort: { quantity: -1 } },
+                    { $limit: 5},
+                    { $lookup: {
+                        from: "products",
+                        localField: "_id",
+                        foreignField: "_id",
+                        as: "productDetails"
+                    }},
+                    {
+                        $project: {
+                            _id: "$_id",
+                            productName:{  $arrayElemAt: ["$productDetails.productName", 0] },
+                            productImg:{ $arrayElemAt: ["$productDetails.image", 0] },
+                        }
+                    }
+
+                ]);
+              
+                const topCategories = await Order.aggregate([
+                    { $match: { 'products.status': 'delivered' } }, // Match orders with delivered products
+                    { $unwind: '$products' }, // Unwind the products array
+                    {
+                        $lookup: { // Lookup to fetch product details
+                            from: 'products',
+                            localField: 'products.productId',
+                            foreignField: '_id',
+                            as: 'product'
+                        }
+                    },
+                    { $unwind: '$product' }, // Unwind the product array
+                    {
+                        $group: { // Group by category and count the number of orders
+                            _id: '$product.category',
+                            orderCount: { $sum: 1 }
+                        }
+                    },
+                    { $sort: { orderCount: -1 } }, // Sort by order count in descending order
+                    {
+                        $lookup: { // Lookup to fetch category details
+                            from: 'categories',
+                            localField: '_id',
+                            foreignField: '_id',
+                            as: 'category'
+                        }
+                    },
+                    { $unwind: '$category' }, // Unwind the category array
+                    { 
+                        $project: { // Project to include only relevant fields
+                            categoryName: '$category.categoryName',
+                            orderCount: 1
+                        }
+                    },
+                    { $sort: { orderCount: -1 } } // Sort categories by order count in descending order
+                ]);
+                
+
+        console.log(topCategories);
+   
+        res.render('dashboard',
+            {
+                orderscount,
+                totalRevenue,
+                productscount,
+                monthlyEarnings,
+                currentMonthRevenue,
+                monthlyUserCounts,
+                topProducts,
+                topCategories
+
+                
+            });
+    } catch (error) {
         console.log(error);
     }
 }
+
 // DASHBOADR SECTON END //
 
 
--- public/admin/assets/js/custom-chart.js
+++ public/admin/assets/js/custom-chart.js
@@ -4,6 +4,9 @@
     /*Sale statistics Chart*/
     if ($('#myChart').length) {
         var ctx = document.getElementById('myChart').getContext('2d');
+        var revenue = JSON.parse($('#revenueData').val());
+        var orders = JSON.parse($('#ordersData').val())
+        var users = JSON.parse($('#usersData').val())
         var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'line',
@@ -12,28 +15,28 @@
             data: {
                 labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
                 datasets: [{
-                        label: 'Sales',
+                        label: 'Revenue',
                         tension: 0.3,
                         fill: true,
                         backgroundColor: 'rgba(44, 120, 220, 0.2)',
                         borderColor: 'rgba(44, 120, 220)',
-                        data: [18, 17, 4, 3, 2, 20, 25, 31, 25, 22, 20, 9]
+                        data: revenue
                     },
                     {
-                        label: 'Visitors',
+                        label: 'Orders',
                         tension: 0.3,
                         fill: true,
                         backgroundColor: 'rgba(4, 209, 130, 0.2)',
                         borderColor: 'rgb(4, 209, 130)',
-                        data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
+                        data: orders
                     },
                     {
-                        label: 'Products',
+                        label: 'Users',
                         tension: 0.3,
                         fill: true,
                         backgroundColor: 'rgba(380, 200, 230, 0.2)',
                         borderColor: 'rgb(380, 200, 230)',
-                        data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
+                        data: users
                     }
 
                 ]
